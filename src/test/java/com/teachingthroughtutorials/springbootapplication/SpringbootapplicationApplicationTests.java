008dfbeb7b7d0becc688c97162bf5118f0ccd7d3
Travis Kent Beste <travis@tencorners.com>
2020-01-25 00:41:56 -0600

    initial commit

---
+++ src/test/java/com/teachingthroughtutorials/springbootapplication/SpringbootapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teachingthroughtutorials.springbootapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
